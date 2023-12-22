02769fb7b4b19feefb73cdbbd71a1f5b8ffb8e74
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2023-12-22 11:36:09 -0300

    att url 'page/'

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -6,6 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', index, name='index'),
     path('post/', post, name='post'),
-    path('page', page, name='page'),
+    path('page/', page, name='page'),
 ]
 
