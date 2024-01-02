9ecfe091b321721f77f0fb95743a4966b732725e
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2024-01-02 11:53:21 -0300

    557

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -123,27 +123,44 @@ class CategoryListView(PostListView):
         })
         return ctx
     
-def category(request, slug):
-    posts = Post.objects.get_published().filter(category__slug=slug)
+# def category(request, slug):
+#     posts = Post.objects.get_published().filter(category__slug=slug)
     
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+#     paginator = Paginator(posts, PER_PAGE)
+#     page_number = request.GET.get("page")
+#     page_obj = paginator.get_page(page_number)
 
-    if len(page_obj) == 0:
-        raise Http404()
+#     if len(page_obj) == 0:
+#         raise Http404()
     
-    page_title = f'{page_obj[0].category.name} - Categoria - '
+#     page_title = f'{page_obj[0].category.name} - Categoria - '
     
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
-            'page_title': page_title,
-        }
-    )
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': page_obj,
+#             'page_title': page_title,
+#         }
+#     )
+
+class TagListView(PostListView):
+    allow_empty = False
     
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset(). filter(
+            tags__slug=self.kwargs.get('slug')
+        )
+        
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        ctx = super().get_context_data(**kwargs)
+        page_title = f'{self.object_list[0].tags.first().name} - Tag - '
+        ctx.update({
+            'page_title':page_title
+        })
+        
+        return ctx
+
 def tag(request, slug):
     posts = Post.objects.get_published().filter(tags__slug=slug)
     
