53e0ed5db7109045022c577e658b0ac1f28104e3
Alexandre <cardoal01@gmail.com>
2024-01-02 10:17:05 -0300

    556

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -2,6 +2,7 @@ from typing import Any
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from blog.models import Post, Page
 from django.http import Http404
@@ -79,32 +80,48 @@ class CreatedByListView(PostListView):
         qs = qs.filter(created_by__pk=self.kwargs.get('author_pk'))
         return qs
     
-def created_by(request, author_pk):
-    user = User.objects.filter(pk=author_pk).first()
+# def created_by(request, author_pk):
+#     user = User.objects.filter(pk=author_pk).first()
     
-    if user is None:
-        raise Http404()
+#     if user is None:
+#         raise Http404()
     
-    posts = Post.objects.get_published().filter(created_by__pk=author_pk)
-    user_full_name = user.username
+#     posts = Post.objects.get_published().filter(created_by__pk=author_pk)
+#     user_full_name = user.username
     
-    if user.first_name:
-        user_full_name = f'{user.first_name} {user.last_name}'
-    page_title = 'Posts de ' + user_full_name + ' - '
+#     if user.first_name:
+#         user_full_name = f'{user.first_name} {user.last_name}'
+#     page_title = 'Posts de ' + user_full_name + ' - '
     
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+#     paginator = Paginator(posts, PER_PAGE)
+#     page_number = request.GET.get("page")
+#     page_obj = paginator.get_page(page_number)
 
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
-            'page_title': page_title,
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': page_obj,
+#             'page_title': page_title,
             
-        }
-    )
+#         }
+#     )
+    
+class CategoryListView(PostListView):
+    allow_empty = False
+    
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(
+            category__slug=self.kwargs.get('slug')
+        )    
+        
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        ctx = super().get_context_data(**kwargs)
+        page_title = f'{self.object_list[0].category.name} - Categoria - '
+        ctx.update({
+            'page_title':page_title
+        })
+        return ctx
     
 def category(request, slug):
     posts = Post.objects.get_published().filter(category__slug=slug)
